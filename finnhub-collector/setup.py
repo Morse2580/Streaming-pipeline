f71347027a609406ed9ca9517d9385b4f700afa6
mnjau <moses.njau@telenetgroup.be>
2023-08-15 20:06:27 +0200

    (local-dev)/ finnhub-collector set-up

---
+++ finnhub-collector/setup.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from setuptools import find_namespace_packages, setup
+
+# Load packages from requirements.txt
+BASE_DIR = Path(__file__).parent
+with open(Path(BASE_DIR, "requirements.txt"), "r") as file:
+    required_packages = [ln.strip() for ln in file.readlines()]
+
+
+# setup.py
+test_packages = [
+    "pytest==7.4.0",
+]
+
+# Define our package
+setup(
+    name="collector_API",
+    version=0.1,
+    description="proj_description",
+    python_requires=">=3.7",
+    packages=find_namespace_packages(),
+    install_requires=[required_packages],
+    extras_require={
+                "dev": test_packages,
+                "test": test_packages
+            }
+)
